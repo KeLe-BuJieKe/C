d74de7d44da310a0b02c5061991a6b29f975b8e4
杰 周 <1941286082@qq.com>
2021-02-16 20:47:46 +0800

    day 6算法题、智力题还未写

---
+++ test_2_15/test_2_15.c
@@ -0,0 +1,90 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//1.以下程序的运行结果是
+//#include<stdio.h>
+//int fun(int a, int b)
+//{
+//	if (a > b)
+//	{
+//		return a + b;
+//	}
+//	else
+//	{
+//		return a - b;
+//	}
+//}
+//int main()
+//{
+//	int a = 4;
+//	int x = 3, y = 8, z = 6, r;
+//	r = fun(fun(x, y), 2 * z);			//r== -17
+//	printf("%d\n", r);
+//	return 0;
+//}
+
+
+
+//式中7*15=133成立，则用的是几进制
+//(7n^0)*(n^1+5n^0)=n^2+3n^1+3n^0
+//解方程x=8或者x=-4(舍弃)
+
+
+
+//下面程序指向后输出的结果是
+//#include<stdio.h>
+//int f(int a)
+//{
+//	int b = 0;
+//	static int c = 3;
+//	a = c++, b++;
+//	//a=c++，b++等价于a=c++和b++
+//	//a=（c++，b++）等价于a=b++，
+//	return a;
+//}
+//int main()
+//{
+//	int  a = 2, k, i;
+//	for (i = 0; i < 2; i++)
+//	{
+//		k = f(a++);			//k==4
+//
+//	}
+//	printf("%d\n", k);
+//	return 0;
+//}
+
+
+
+//下面程序指向后输出的结果是
+//#include<stdio.h>
+//int x = 3;
+//void inc()
+//{
+//	static int x = 1;
+//	x *=(x + 1);
+//	printf("%d", x);		//2 6
+//	return;
+//}
+//int main()
+//{
+//	int i;
+//	for (i = 1; i < x; i++)
+//	{
+//		inc();
+//	}
+//	return 0;
+//}
+
+
+
+//算法题
+//编写函数，要求逆置单链表（不带头结点）
+
+//后期填坑
+
+
+
+//智力题
+//假设你站在镜子前，抬起左手，抬起右手，看着镜子中的自己。当你抬起左手时，镜中的自己抬起的似乎是右手。
+//可是当你仰头时，镜中的自己也在仰头，而不是低头。为什么镜子中的影像似乎颠倒了左右，却没有颠倒上下？
