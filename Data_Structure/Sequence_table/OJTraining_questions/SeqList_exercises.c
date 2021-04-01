4bb0a23c7a89735e8c5170d6143ef7029ef639fb
杰 周 <1941286082@qq.com>
2021-04-01 22:35:47 +0800

    🐱‍🏍

--- Data_Structure/Sequence_table/OJTraining_questions/SeqList_exercises.c
+++ Data_Structure/Sequence_table/OJTraining_questions/SeqList_exercises.c
@@ -198,7 +198,7 @@
 
 
 /*题目：只出现一次的数字Ⅲ
-给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 任意顺序 返回答案。
+给定一个整数数组 nums，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按任意顺序返回答案。
 进阶：你的算法应该具有线性时间复杂度。你能否仅使用常数空间复杂度来实现？
 示例 1：
 输入：nums = [1,2,1,3,2,5]
@@ -218,7 +218,7 @@
  */
 int* singleNumber(int* nums, int numsSize, int* returnSize)
 {
-    //按照上题的思路继续写
+    //按找上题思路
     //1.首先把所有数异或，得到两个只出现一次数异或的结果
     int ret = 0;
     for (int i = 0; i < numsSize; ++i)
@@ -229,15 +229,16 @@ int* singleNumber(int* nums, int numsSize, int* returnSize)
     }
 
     //2.我们开始想方法将它们分离
-    //2.1首先找出ret二进制里面第M位为1的
-    //说明x1和x2的第M位不相同，一个为0，一个为1
+    //2.1首先找出ret二进制第一个二进制1是第几位(M)
+    //说明x1和x2的第M位二进制不相同，一个为0，一个为1
     int M = 0;
     while (M < 32)
     {
         /*假设  x1为5   x2为3
                 0101    0011
                 5^3==0110
-                异或的俩个数一定至少会有一位为1，找到这个1所在第几位
+                不可能会出现ret=0的情况，因为如果要出现这种情况，必须x1与x2相同
+                异或的俩个数一定至少会有一位二进制为1，找到这个1所在第几位
         */
         if ((ret >> M) & 1)      //如果找到有一位为1，则退出循环，否则M++
         {
@@ -270,3 +271,72 @@ int* singleNumber(int* nums, int numsSize, int* returnSize)
     *returnSize = 2;
     return retArray;
 }
+
+
+
+
+/*
+189. 旋转数组
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+进阶：
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+示例 1:
+输入: nums = [1,2,3,4,5,6,7], k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+
+输入：nums = [-1,-100,3,99], k = 2
+输出：[3,99,-1,-100]
+解释: 
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+链接：https://leetcode-cn.com/problems/rotate-array/
+
+//方法一：会超出时间限度
+void rotate(int* nums, int numsSize, int k)
+{
+    
+    while(k--)
+    {
+        int temp=nums[numsSize-1];
+        for(int i=numsSize-1;i>0;i--)
+        {
+            nums[i]=nums[i-1];
+        }
+        nums[0]=temp;
+    }
+}
+
+//方法二：先整体翻转在局部翻转-----类似（翻转字符串）
+void swap(int*x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+void reverse(int *nums,int start,int end)
+{
+    for(;start<end;start++,end--)
+    {
+        swap(&nums[start],&nums[end]);
+    }
+}
+
+void rotate(int* nums, int numsSize, int k)
+{
+    reverse(nums,0,numsSize-1);     //1,2,3,4,5,6,7先整体翻转7，6，5，4，3，2，1
+                                    //再局部翻转 [0,(k%numsSize)-1] 区间的元素
+    reverse(nums,0,(k%numsSize)-1); //5 6 7 4 3 2 1
+                                    //再局部翻转 [(k%numsSize)，numsSize-1] 区间的元素
+    reverse(nums,(k%numsSize),numsSize-1);//5 6 7 1 2 3 4
+}
+
+*/
+
+
+
