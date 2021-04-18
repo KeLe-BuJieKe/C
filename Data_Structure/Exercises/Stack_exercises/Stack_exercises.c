627a4b3f0b6543b7960c6136cd936608a4febe3e
杰 周 <1941286082@qq.com>
2021-04-18 15:08:10 +0800

    🚩

--- Data_Structure/Exercises/Stack_exercises/Stack_exercises.c
+++ Data_Structure/Exercises/Stack_exercises/Stack_exercises.c
@@ -381,3 +381,51 @@ void myStackFree(MyStack* obj)
 	free(obj);
 }
 */
+
+//C++stl µœ÷
+/*
+class MyStack {
+public:
+	// Initialize your data structure here. 
+	queue<int>q1;
+	queue<int>q2;
+
+	MyStack()
+	{
+
+	}
+
+	// Push element x onto stack.
+	void push(int x)
+	{
+		q2.push(x);
+		while (!q1.empty())
+		{
+			q2.push(q1.front());
+			q1.pop();
+		}
+		swap(q1, q2);
+	}
+
+	// Removes the element on top of the stack and returns that element.
+	int pop()
+	{
+		int cur = q1.front();
+		q1.pop();
+		return cur;
+	}
+
+	// Get the top element. 
+	int top()
+	{
+		return q1.front();
+	}
+
+	// Returns whether the stack is empty. 
+	bool empty()
+	{
+		return q1.empty();
+	}
+};
+
+*/
