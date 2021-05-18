1b24997a559282517dfacd7791112ec02b97dab6
杰 周 <1941286082@qq.com>
2021-05-18 16:25:01 +0800

    -

--- LeetCode/Sort_exercise/Sort_exercise.cpp
+++ LeetCode/Sort_exercise/Sort_exercise.cpp
@@ -144,4 +144,122 @@ int* sortArray(int* nums, int numsSize, int* returnSize)
     memcpy(array,nums,sizeof(int)*numsSize);
     return array;
 }
+
+//方法三：堆排序
+
+void swap(int *x,int*y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+void adjustBigHead(int*array,int parent,const int size)
+{
+    int child=parent*2+1;       //左孩子
+    while(child<size)
+    {
+        if(child+1<size&&array[child]<array[child+1])
+        {
+            child++;
+        }
+
+        if(array[child]>array[parent])
+        {
+            swap(&array[child],&array[parent]);
+            parent=child;
+            child=parent*2+1;
+        }
+        else
+        {
+            break;
+        }
+
+    }
+}
+
+void HeapSort(int*array,const int size)
+{
+    for(int i=(size-1)/2;i>=0;i--)
+    {
+        adjustBigHead(array,i,size);
+    }
+
+    int end=size-1;
+    while(end>0)
+    {
+        swap(&array[end],&array[0]);
+        adjustBigHead(array,0,end);
+        --end;
+    }
+}
+
+int* sortArray(int* nums, int numsSize, int* returnSize)
+{
+    *returnSize=numsSize;
+    int *array=(int*)malloc(sizeof(int)*numsSize);
+    HeapSort(nums,numsSize);
+    memcpy(array,nums,sizeof(int)*numsSize);
+    return array;
+}
+
+//方法四：归并排序
+void MergeSort(int* array, int left, int right, int* tempArr)
+{
+    if (left >= right)
+    {
+        return;
+    }
+
+    int mid = (left + right) >> 1;
+    //[left,min]  [mid+1,right]
+    MergeSort(array, left, mid, tempArr);
+    MergeSort(array, mid+1, right, tempArr);
+
+    int begin1 = left;
+    int end1 = mid;
+    int begin2 = mid + 1;
+    int end2 = right;
+    int i = left;
+
+    //两段有序子区间归并给tempArr，并拷贝回给array
+    //只要有一个数组内的元素空了，就跳出循环
+    //出来后把另外一个还未空的数组的元素放进tempArr中
+    while (begin1 <= end1 && begin2 <= end2)
+    {
+        if (array[begin1] < array[begin2])
+        {
+            tempArr[i++] = array[begin1++];
+        }
+        else
+        {
+            tempArr[i++] = array[begin2++];
+        }
+    }
+
+    while (begin1 <= end1)
+    {
+        tempArr[i++] = array[begin1++];
+    }
+
+    while (begin2 <= end2)
+    {
+        tempArr[i++] = array[begin2++];
+    }
+
+    //拷贝给array数组
+    for (int j = left; j <= right; j++)
+    {
+        array[j] = tempArr[j];
+    }
+}
+
+int* sortArray(int* nums, int numsSize, int* returnSize)
+{
+    *returnSize=numsSize;
+    int *array=(int*)malloc(sizeof(int)*numsSize);
+    MergeSort(nums,0,numsSize-1,array);
+    free (array);
+    return nums;
+}
 */
